f9f1a2ed331c0af11f304a8c6c562ae90e9c43a7
Sumeet0819 <157307366+Sumeet0819@users.noreply.github.com>
2025-12-30 23:35:21 +0530

    cocktails section completed with animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import gsap, { ScrollTrigger , SplitText} from 'gsap/all'
 import Hero from './components/Hero'
+import Cocktails from './components/Cocktails'
 
 gsap.registerPlugin(ScrollTrigger,SplitText)
 
@@ -10,6 +11,7 @@ const App = () => {
   <main>
     <Navbar/>
     <Hero/>
+    <Cocktails/>
   </main>
   )
 }
